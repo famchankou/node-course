baa292f0fa8cc7a534ad79b92b1b26f629e81059
AFamchankou <Aliaksandr_Famchankou@epam.com>
2018-06-05 21:08:00 +0300

    Express added

--- app.js
+++ app.js
@@ -2,21 +2,25 @@ import * as Path from "path";
 import config from "./config";
 import { User, Product } from "./models";
 import { Importer, DirWatcher } from "./modules";
+import Express from "express";
 
+const app = Express();
 const DATA_DIR = Path.join(__dirname, "data");
 
-(() => {
-    let user = new User();
-    let product = new Product();
+app.get('/', (req, res) => res.send('Hello World!'))
 
-    let importer = new Importer();
-    let dirWatcher = new DirWatcher();
-    
-    dirWatcher.watch(DATA_DIR, 3000);
-    dirWatcher.on("dirwatcher:changed", updatedFiles => {
-        importer
-            .import(DATA_DIR, updatedFiles)
-            .then(data => console.log("Data: ", JSON.stringify(data)))
-            .catch(error => console.error("Error: ", error));
-    });
-})();
+let user = new User();
+let product = new Product();
+
+let importer = new Importer();
+let dirWatcher = new DirWatcher();
+
+dirWatcher.watch(DATA_DIR, 3000);
+dirWatcher.on("dirwatcher:changed", updatedFiles => {
+    importer
+        .import(DATA_DIR, updatedFiles)
+        .then(data => console.log("Data: ", JSON.stringify(data)))
+        .catch(error => console.error("Error: ", error));
+});
+
+module.exports = app;
