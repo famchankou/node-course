53ef612b68538e514213f05e3a92d4b71e76629e
AFamchankou <Aliaksandr_Famchankou@epam.com>
2018-06-25 20:16:13 +0300

    Config updates

--- database/sequelize.js
+++ database/sequelize.js
@@ -15,10 +15,10 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-readdirSync(__dirname)
-    .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+readdirSync(Path.join(__dirname, "models"))
+    .filter((file) => (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
     .forEach((file) => {
-        const model = sequelize.import(path.join(__dirname, file));
+        const model = sequelize.import(Path.join(__dirname, "models", file));
         db[model.name] = model;
     });
 
