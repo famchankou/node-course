f99d3984095dc1d37e44029ca0efd1088df0a397
AFamchankou <Aliaksandr_Famchankou@epam.com>
2018-07-15 21:42:38 +0300

    feat: Models properties validation added

--- database/mongodb/models/city.js
+++ database/mongodb/models/city.js
@@ -2,12 +2,46 @@ import Mongoose from "mongoose";
 const Schema = Mongoose.Schema;
 
 const City = new Schema({
-    name: { type: String, required: true, unique: false },
-    country: String,
+    name: {
+        type: String,
+        validate: {
+            validator: v => /^[A-Z][a-z]*$/.test(v),
+            message: '{VALUE} is not a valid city name!'
+        },
+        required: [true, 'City name is required']
+    },
+    country: {
+        type: String,
+        validate: {
+            validator: v => /^[A-Z][a-z]*$/.test(v),
+            message: '{VALUE} is not a valid country name!'
+        },
+        required: [true, 'Country name is required']
+    },
     capital: Boolean,
     location: {
-        lat: Number,
-        long: Number
+        lat: {
+            type: Number,
+            validate: {
+                validator: v => {
+                    var num = parseFloat((v + "").replace(/^(.*\.\d\d)\d*$/, '$1'));
+                    return num > 0 && num < 100;
+                },
+                message: '{VALUE} is not valid latitude!'
+            },
+            required: [true, 'Latitude is required']
+        },
+        long: {
+            type: Number,
+            validate: {
+                validator: v => {
+                    var num = parseFloat((v + "").replace(/^(.*\.\d\d)\d*$/, '$1'));
+                    return num > 0 && num < 100;
+                },
+                message: '{VALUE} is not valid longitude!'
+            },
+            required: [true, 'Longitude is required']
+        }
     },
     lastModifiedDate: String,
     created_at: { type: Date, default: Date.now },
--- database/mongodb/models/product.js
+++ database/mongodb/models/product.js
@@ -4,9 +4,30 @@ const Schema = Mongoose.Schema;
 const Product = new Schema({
     id: { type: String, unique: true, required : true },
     userId: Number,
-    name: String,
-    sku: String,
-    basePrice: Number,
+    name: {
+        type: String,
+        validate: {
+            validator: v => /^[A-Z][a-z]*$/.test(v),
+            message: '{VALUE} is not a valid product name!'
+        },
+        required: [true, 'Product name is required']
+    },
+    sku: {
+        type: String,
+        validate: {
+            validator: v => /[A-Za-z]*[0-9]*\w*/.test(v),
+            message: '{VALUE} is not a valid sku!'
+        },
+        required: [true, 'Sku is required']
+    },
+    basePrice: {
+        type: Number,
+        validate: {
+            validator: v => /\D*/.test(v),
+            message: '{VALUE} is not valid base price!'
+        },
+        required: [true, 'Base price is required']
+    },
     productType: String,
     lastModifiedDate: String,
     created_at: { type: Date, default: Date.now },
